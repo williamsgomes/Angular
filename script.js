9eadb5774674017aaf755532b1e49b2454d0601c
William Gomes <wsgpositivo@gmail.com>
2018-03-18 19:17:02 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function ListaComprasController($scope) {
-    $scope.itens = [
-        {produto: 'Leite', quantidade: 2, comprado: false},
-        {produto: 'Cerveja', quantidade: 12, comprado: false}
-    ];
-}
+var aplication = angular.module("myApp", []);
+aplication.controller("myCtrl", function($scope){
+	$scope.produtos = ["Leite", "Café", "Feijão", "Açucar"];
+	$scope.actionNovoProduto = function(){
+		$scope.produtos.push($scope.novoProduto);
+	};
+})

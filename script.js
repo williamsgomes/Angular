54be993fd7159de0a0c4709c19bc803e06565d40
williamsgomes <wsgpositivo@gmail.com>
2018-03-18 20:45:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var aplication = angular.module("myApp", []);
-aplication.controller("myCtrl", function($scope){
-	$scope.produtos = ["Leite", "Café", "Feijão", "Açucar"];
-	$scope.actionNovoProduto = function(){
-		$scope.produtos.push($scope.novoProduto);
-	};
-})
